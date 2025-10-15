c5a44075ff34f47ad0449b588f90b9cee0c3bde3
a-a-tel <anouaranthonytel@gmail.com>
2025-10-15 16:45:18 +0200

    refactored for readability

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include <iostream>
 #include <traffic_anal/requests.hpp>
 
-int main()
+namespace req = traffic_anal::requests;
+
+int main(int argc, char **argv)
 {
-    auto [frc, currentSpeed, freeFlowSpeed, currentTravelTime, freeFlowTravelTime, confidence, roadClosure] = traffic_anal::getSegmentData(51.84605,5.86274);
+    auto [frc,
+        currentSpeed,
+        freeFlowSpeed,
+        currentTravelTime,
+        freeFlowTravelTime,
+        confidence,
+        roadClosure]
+            = req::parseSegmentData(req::getSegmentData(40.7603901911296, -73.97380128799641));
+
+
     std::cout << frc << std::endl;
     std::cout << currentSpeed << std::endl;
     std::cout << freeFlowSpeed << std::endl;
     std::cout << currentTravelTime << std::endl;
     std::cout << freeFlowTravelTime << std::endl;
     std::cout << confidence << std::endl;
-    std::cout << roadClosure << std::endl;
+    std::cout << std::boolalpha << roadClosure << std::endl;
 
     return 0;
 }
