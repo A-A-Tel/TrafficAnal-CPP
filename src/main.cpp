3cf866a349ee8c1bc810a7aa9ef6cbfac7e4fd18
a-a-tel <anouaranthonytel@gmail.com>
2025-10-16 13:41:46 +0200

    added coords parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,46 @@
+#include <boost/algorithm/string.hpp>
 
 #include <iostream>
+#include <regex>
+
 #include <traffic_anal/requests.hpp>
 
 namespace req = traffic_anal::requests;
 
-int main(int argc, char **argv)
+
+/**
+ * This funciton parses out 2 coordinates from a string using <code>(\-?\d+)(\.\d+)?</code>.
+ *
+ * @param input The input string of your coordinates
+ * @param output The output vector for your coordinates
+ * @throws std::invalid_argument if there are more/less than 2 coordinates.
+ */
+void filter_coords_input(const std::string& input, std::vector<double>& output) {
+
+    const std::regex pattern{R"((\-?\d+)(\.\d+)?)"};
+
+    for (std::sregex_iterator it(input.begin(), input.end(), pattern), end; it != end; ++it) {
+        output.push_back(std::strtod(it->str().c_str(), nullptr));
+    }
+
+    if (output.size() != 2) throw std::invalid_argument("Too many coordinates.");
+
+}
+
+int main()
 {
-    auto [frc,
-        currentSpeed,
-        freeFlowSpeed,
-        currentTravelTime,
-        freeFlowTravelTime,
-        confidence,
-        roadClosure]
-            = req::parseSegmentData(req::getSegmentData(40.7603901911296, -73.97380128799641));
+    std::string input;
+    std::cout <<  "Please enter coordinates: ";
+    std::getline(std::cin, input);
+
+    std::vector<double> coords;
+    filter_coords_input(input, coords);
 
 
-    std::cout << frc << std::endl;
-    std::cout << currentSpeed << std::endl;
-    std::cout << freeFlowSpeed << std::endl;
-    std::cout << currentTravelTime << std::endl;
-    std::cout << freeFlowTravelTime << std::endl;
-    std::cout << confidence << std::endl;
-    std::cout << std::boolalpha << roadClosure << std::endl;
+
+
+
+
 
     return 0;
 }
--- src/traffic_anal/requests.cpp
+++ src/traffic_anal/requests.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <traffic_anal/requests.hpp>
+#include <logger/Logger.hpp>
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
@@ -11,7 +12,6 @@
 #include <boost/asio/ip/tcp.hpp>
 
 #include <boost/json.hpp>
-
 
 #include <iostream>
 
@@ -83,7 +83,7 @@ namespace traffic_anal::requests {
 
         // Test data
         // json_str = R"({"flowSegmentData":{"frc":"FRC1","currentSpeed":25,"freeFlowSpeed":25,"currentTravelTime":59,"freeFlowTravelTime":59,"confidence":1,"roadClosure":false,"coordinates":{"coordinate":[{"latitude":51.842671952025697,"longitude":5.866043378102120},{"latitude":51.842611622086366,"longitude":5.865791250454691},{"latitude":51.842564652712788,"longitude":5.865612883555173},{"latitude":51.842331306895552,"longitude":5.864658017145615},{"latitude":51.842174395912245,"longitude":5.863948572861034},{"latitude":51.842073827595044,"longitude":5.863240469680932},{"latitude":51.842038975597490,"longitude":5.862905193553985},{"latitude":51.842004071786867,"longitude":5.862301696525464},{"latitude":51.841994698503697,"longitude":5.862083096490721},{"latitude":51.841990659187758,"longitude":5.861971784816575},{"latitude":51.841998737819374,"longitude":5.861462165103603},{"latitude":51.842017484381984,"longitude":5.861065198169342},{"latitude":51.842038975597490,"longitude":5.860847939239079},{"latitude":51.842060415016697,"longitude":5.860666890130545},{"latitude":51.842104692045829,"longitude":5.860433537946165},{"latitude":51.842160983363840,"longitude":5.860193480239303}]},"@version":"4"}})";
-        std::cout << json_str << std::endl;
+        logger::Logger::get_instance()->log(json_str);
 
         try {
             const json::value jv = json::parse(json_str);
